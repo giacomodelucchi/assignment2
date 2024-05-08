a2f2dbcbc0a0607d96a9f978ff0468002ae5a9d7
NiccolCarl <niccolcarl@gmail.com>
2024-05-08 11:48:59 +0200

    preparazione maven

---
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -0,0 +1,15 @@
+package it.unipd.mtss;
+
+import org.junit.Test;
+
+public class RomanPrinterTest {
+    @Test
+    public void testPrint() {
+
+    }
+
+    @Test
+    public void testPrintAsciiArt(){
+        
+    }
+}
